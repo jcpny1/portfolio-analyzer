4ba093cb51fae80864917127438ab38597154790
John Pfingst <jpfingst@outlook.com>
2017-10-21 18:25:02 -0400

    Setup external price api call.

--- Gemfile
+++ Gemfile
@@ -5,13 +5,10 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'rails', '~> 5.1.4'  # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'sqlite3'  # Use sqlite3 as the database for Active Record
+gem 'puma', '~> 3.7'  # Use Puma as the app server
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.4'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
-# Use Puma as the app server
-gem 'puma', '~> 3.7'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
@@ -26,21 +23,21 @@ gem 'puma', '~> 3.7'
 # gem 'rack-cors'
 
 # JCP added:
-gem 'foreman', '~> 0.84.0'
 gem 'active_model_serializers', '~> 0.10.0'
+gem 'dotenv-rails'
+gem 'foreman', '~> 0.84.0'
+gem 'faraday'  # for GET and POST
 # end JCP added
-
-group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'pry'
-end
 
 group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
+  gem 'spring'  # speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :development, :test do
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'pry'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/daily_trades_controller.rb
+++ app/controllers/daily_trades_controller.rb
@@ -1,12 +1,56 @@
 class DailyTradesController < ApplicationController
+
+  # https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=demo
+
+  # {
+  #   "Meta Data"=>
+  #   {
+  #     "1. Information"=>"Intraday (1min) prices and volumes",
+  #     "2. Symbol"=>"MSFT",
+  #     "3. Last Refreshed"=>"2017-10-20 16:00:00",
+  #     "4. Interval"=>"1min",
+  #     "5. Output Size"=>"Compact",
+  #     "6. Time Zone"=>"US/Eastern"
+  #   },
+  #   "Time Series (1min)"=>
+  #   {
+  #     "2017-10-20 16:00:00"=>
+  #     {
+  #       "1. open"=>"78.7000",
+  #       "2. high"=>"78.8100",
+  #       "3. low"=>"78.6950",
+  #       "4. close"=>"78.8100",
+  #       "5. volume"=>"2663315"
+  #     },
+  #     "2017-10-20 15:59:00"=>
+  #     {
+  #       "1. open"=>"78.7000",
+  #       "2. high"=>"78.7200",
+  #       "3. low"=>"78.6900",
+  #       "4. close"=>"78.7000",
+  #       "5. volume"=>"320215"
+  #     },
+  #     .
+  #     .
+  #     .
+  #   }
+  # }
+
   # Retrieve the latest closing price for the supplied symbols.
   def last_close
     symbols = params['symbols'].split(',').uniq
     last_close_prices = {}
     symbols.each { |symbol|
-      stock_symbol = StockSymbol.find_by(name: symbol)
-      last_trade_record = stock_symbol.daily_trades.order(trade_date: :desc).first
-      last_close_prices[symbol] = last_trade_record.close_price if last_trade_record.present?
+      resp = Faraday.get("https://www.alphavantage.co/query") do |req|
+        req.params['apikey']   = ENV['ALPHA_VANTAGE_API_KEY']
+        req.params['function'] = 'TIME_SERIES_INTRADAY'
+        req.params['interval'] = '1min'
+        req.params['symbol']   = symbol
+      end
+      response = JSON.parse(resp.body)
+      last_close_prices[symbol] = {}
+      last_close_prices[symbol]['header'] = response['Meta Data']
+      last_close_prices[symbol]['tick']   = response['Time Series (1min)'].first
     }
     render json: last_close_prices
   end
