ba5fe123146b6842d1bd5a30f6a495c0946abc73
John Pfingst <jpfingst@outlook.com>
2017-10-29 00:03:49 -0400

    Enhance model validations.

--- app/models/open_position.rb
+++ app/models/open_position.rb
@@ -1,4 +1,7 @@
 class OpenPosition < ApplicationRecord
   belongs_to :portfolio, inverse_of: :open_positions
   belongs_to :stock_symbol
+  validates :cost, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+  validates :date_acquired, presence: { message: "is not valid" }
 end
