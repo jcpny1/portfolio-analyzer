6ca280c1e7a3734a8e981a781802afcc80dc2083
John Pfingst <jpfingst@outlook.com>
2017-11-17 16:52:39 -0500

    Change DJIA and Refresh Time from Button to Header<span> so they don't show as clickable.

--- client/src/components/Headlines.js
+++ client/src/components/Headlines.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Header, Table} from 'semantic-ui-react';
+import {Button, Header, Label, Table} from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import Fmt from '../utils/formatter';
 
@@ -27,9 +27,9 @@ export const Headlines = (props) => {
       <Header size='medium' color='purple' style={{marginBottom:0, marginLeft:'3px'}}>
         Headline News
         <span disabled style={{float:'right', fontSize:'70%'}}>
-          <Button content={Fmt.index('DJIA', djia)} title='Dow Jones Industrial Average' size='tiny' inverted compact/>
+          <Header as='span' content={Fmt.index('DJIA', djia)} title='Dow Jones Industrial Average' size='tiny' color='purple'/>
           &emsp;&emsp;
-          <Button content={refreshTime.toLocaleTimeString("en-US")} title='Refresh time' size='tiny' inverted compact/>
+          <Header as='span' content={refreshTime.toLocaleTimeString("en-US")} title='Refresh time' size='tiny' color='purple'/>
           &emsp;&emsp;
           <Button disabled content='Refresh: 1 min' icon='refresh' title='Refresh headlines' compact inverted size='tiny' style={{paddingRight:'3px'}} onClick={() => props.refreshHeadlines()}/>
         </span>
