c794940746c170b17381fcd4989bf7d2794f8399
John Pfingst <jpfingst@outlook.com>
2017-11-18 11:42:39 -0500

    Test directory not needed.Tag Gemfile.lock to github as a generated file.

--- test/controllers/headlines_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class HeadlinesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/trade_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TradeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
