179b4e9f926519b777ca5d1a7d5b2280cb8eaf4b
John Pfingst <jpfingst@outlook.com>
2018-03-19 10:45:51 -0400

    Add test for new monthly series api.

--- spec/controllers/series_controller_spec.rb
+++ spec/controllers/series_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe SeriesController, type: :controller do
-
+  describe "GET monthly series" do
+    it "returns monthly series data" do
+      request.accept = "application/json"
+      get :monthly_series, { :params => { symbols: 'DIA' }, format: :json }
+      pr = JSON.parse(response.body)
+      expect(response).to have_http_status(:success)
+      expect(pr['data'].length).to be > 0
+      expect(pr['data'][0]['attributes']['adjusted-close-price']).to eq('249.7448')
+      expect(pr['included'].length).to be > 0
+      expect(pr['included'][0]['attributes']['symbol']).to eq('DIA')
+    end
+  end
 end
