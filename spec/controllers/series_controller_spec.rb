84a7c4cb5dc75f5bd35a5af621d86cd8b286d09c
John Pfingst <jpfingst@outlook.com>
2018-03-20 16:46:58 -0400

    Fix rspec test error.

--- spec/controllers/series_controller_spec.rb
+++ spec/controllers/series_controller_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe SeriesController, type: :controller do
   describe "GET monthly series" do
     it "returns monthly series data" do
       request.accept = "application/json"
-      get :monthly_series, { :params => { symbols: 'DIA' }, format: :json }
+      get :monthly_series, { :params => { symbols: 'AAPL' }, format: :json }
       pr = JSON.parse(response.body)
       expect(response).to have_http_status(:success)
       expect(pr['data'].length).to be > 0
-      expect(pr['data'][0]['attributes']['adjusted-close-price']).to eq('249.7448')
+      expect(pr['data'][0]['attributes']['adjusted-close-price']).to eq('128.45')
       expect(pr['included'].length).to be > 0
-      expect(pr['included'][0]['attributes']['symbol']).to eq('DIA')
+      expect(pr['included'][0]['attributes']['symbol']).to eq('AAPL')
     end
   end
 end
