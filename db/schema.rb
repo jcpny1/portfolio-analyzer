310e55368b8e86ce1f8d3fd32fe226939b05e833
John Pfingst <jpfingst@outlook.com>
2018-03-24 22:01:18 -0400

    Update schema.rb. Missed it in prior checkin.

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180313194331) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["symbol"], name: "instruments_by_symbol", unique: true
   end
 
   create_table "portfolios", force: :cascade do |t|
