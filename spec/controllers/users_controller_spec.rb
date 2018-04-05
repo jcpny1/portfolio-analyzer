19eba8532334ffc6daa6c10cb2c0205f6f3789ea
John Pfingst <jpfingst@outlook.com>
2018-04-05 16:54:20 -0400

    Increase user_controller test coverage.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  # before(:each) do
+  #   @user = create(:user)
+  # end
+
+  # TODO: These test will need to be fixed when user is no longer hardcoded to 1,'guest'.
+
+  describe "GET #show" do
+    it "returns one user" do
+      request.accept = "application/json"
+      get :show, { :params => { id: 1 }, format: :json }
+      pr = JSON.parse(response.body)
+      expect(response).to have_http_status(:success)
+      expect(pr['data'].length).to be > 0
+      expect(pr['data']['id']).to eq('1')
+    end
+  end
+
+  describe "PATCH #update" do
+    it "updates an existing user" do
+      request.accept = "application/json"
+      patch :update, { params: { id: 1, user: { locale: 'krypton-en' }}, format: :json }
+      pr = JSON.parse(response.body)
+      expect(response).to have_http_status(:success)
+      expect(pr['data']['attributes']['locale']).to eq('krypton-en')
+    end
+  end
 end
