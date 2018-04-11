564d8082f0b05acf354ff2806d5ed2b302fd9562
John Pfingst <jpfingst@outlook.com>
2018-04-11 17:52:46 -0400

    First cut at Jest testing.

--- client/_test_/actions/App.test.js
+++ client/_test_/actions/App.test.js
@@ -3,18 +3,36 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import ShallowRenderer from 'react-test-renderer/shallow';
 import rootReducer from '../../src/reducers';
 import {WrapperApp} from '../../src/App';
-import * as actions from '../../src/reducers/userReducer';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-// });
+import HeadlinesPage from '../../src/containers/HeadlinesPage';
+import PortfoliosPage from '../../src/containers/PortfoliosPage';
+import PortfolioChartPage from '../../src/containers/PortfolioChartPage';
+import PositionsPage from '../../src/containers/PositionsPage';
+import PositionEditPage from '../../src/containers/PositionEditPage';
 
-it('renders without crashing', () => {
+import {Portfolios} from '../../src/components/Portfolios';
+import {Positions} from '../../src/components/Positions';
+
+import Decimal from '../../src/classes/Decimal';
+import Portfolio from '../../src/classes/Portfolio';
+import Position from '../../src/classes/Position';
+
+import * as PositionAction from '../../src/actions/positionActions';
+import * as PortfolioReducer from '../../src/reducers/portfolioReducer';
+import * as UserReducer from '../../src/reducers/userReducer';
+
+const myMock = jest.fn();
+const decimalValue = new Decimal(0.0, 'currency');
+const myPortfolio = new Portfolio('1', 'test portfolio');
+const myPosition = new Position('1', '1', 100.0, 1.0, '2018-01-01');
+myPortfolio._positions.push(myPosition);
+
+it('renders Home page', () => {
   const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f));
-    const div = document.createElement('div');
+  const div = document.createElement('div');
   ReactDOM.render (
     <Provider store={store}>
       <WrapperApp />
@@ -23,11 +41,123 @@ it('renders without crashing', () => {
   );
 });
 
+it('renders HeadlinesPage', () => {
+  const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f));
+  const div = document.createElement('div');
+  ReactDOM.render (
+    <Provider store={store}>
+      <HeadlinesPage />
+    </Provider>,
+    div
+  );
+});
+
+it('renders PortfoliosPage', () => {
+  const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f));
+  const div = document.createElement('div');
+  ReactDOM.render (
+    <Provider store={store}>
+      <PortfoliosPage/>
+    </Provider>,
+    div
+  );
+});
+
+it('renders PortfolioChartPage', () => {
+  const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f));
+  const div = document.createElement('div');
+  ReactDOM.render (
+    <Provider store={store}>
+      <PortfolioChartPage portfolio={myPortfolio} iconName='chart line' iconColor='blue' tooltip='Chart portfolio'/>
+    </Provider>,
+    div
+  );
+});
+
+it('renders PositionsPage', () => {
+  const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f));
+  const div = document.createElement('div');
+  const match = {params:{id: '1'}};
+  ReactDOM.render (
+    <Provider store={store}>
+      <PositionsPage match={match}/>
+    </Provider>,
+    div
+  );
+});
+
+it('renders PositionEditPage', () => {
+  const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f));
+  const div = document.createElement('div');
+  const match = {params:{id: '1'}};
+  ReactDOM.render (
+    <Provider store={store}>
+      <PositionEditPage position={myPosition} iconName='edit' iconColor='blue' tooltip='Edit position'/>
+    </Provider>,
+    div
+  );
+});
+
+it('renders Portfolios', () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<Portfolios portfolios={[myPortfolio]} updatingPortfolio={false} totalCost={decimalValue} totalDayChange={decimalValue} totalGainLoss={decimalValue} totalMarketValue={decimalValue} refreshPortfolios={myMock} onClickRemove={myMock} onClickColHeader={myMock} sortColName={'name'} sortDirection={'ascending'} userLocale={'en-US'}/>);
+  const result = renderer.getRenderOutput();
+});
+
+it('renders Positions', () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<Positions portfolio={myPortfolio} updatingPortfolio={false} portfolioRefresh={myMock} onClickSubmit={myMock} onClickRemove={myMock} onClickColHeader={myMock} sortColName={'symbol'} sortDirection={'ascending'} userLocale={'en-US'}/>);
+  const result = renderer.getRenderOutput();
+});
+
 describe('actions', () => {
-  it('should have action updatingUser()', () => {
-    const expectedAction = {
-      type: actions.userActions.UPDATING
-    }
-    expect(actions.updatingUser()).toEqual(expectedAction)
+  it('should have action User actions', () => {
+    expect(UserReducer.errorUser()).toHaveProperty('type');
+    expect(UserReducer.updateUser()).toHaveProperty('type');
+    expect(UserReducer.updatingUser()).toHaveProperty('type');
+    expect(UserReducer.warnUser()).toHaveProperty('type');
+  })
+
+  it('should have action updatingPortfolio()', () => {
+    expect(PortfolioReducer.addPortfolio()).toHaveProperty('type');
+    expect(PortfolioReducer.deletePortfolio()).toHaveProperty('type');
+    expect(PortfolioReducer.errorPortfolio()).toHaveProperty('type');
+    expect(PortfolioReducer.updatePortfolio()).toHaveProperty('type');
+    expect(PortfolioReducer.updateAllPortfolio()).toHaveProperty('type');
+    expect(PortfolioReducer.updatingPortfolio()).toHaveProperty('type');
+    expect(PortfolioReducer.warnPortfolio()).toHaveProperty('type');
+  })
+
+  it('should have Position actions', () => {
+    expect(PositionAction.positionAdd(myPosition, myMock)).toBeDefined();
+    expect(PositionAction.positionDelete('1', '1', myMock)).toBeDefined();
+    expect(PositionAction.positionsSort([myPortfolio], 'name', myMock)).toBeDefined();
+    expect(PositionAction.positionUpdate(myPosition, myMock)).toBeDefined();
   })
 });
+
+// test('root page renders', () => {
+//   const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f));
+//   const div = document.createElement('div');
+//   const component = renderer.create(
+//     <Provider store={store}>
+//       <WrapperApp />
+//     </Provider>,
+//     div
+//   );
+// });
+
+  // let tree = component.toJSON();
+  // expect(tree).toMatchSnapshot();
+
+  // // manually trigger the callback
+  // tree.props.onMouseEnter();
+  // // re-rendering
+  // tree = component.toJSON();
+  // expect(tree).toMatchSnapshot();
+  //
+  // // manually trigger the callback
+  // tree.props.onMouseLeave();
+  // // re-rendering
+  // tree = component.toJSON();
+  // expect(tree).toMatchSnapshot();
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,39 +1,39 @@
 import Fmt from '../utils/formatter';
 
-export const userActions = {
+const userAction = {
   ERROR   : 'USER_ERROR',
   UPDATE  : 'USER_UPDATE',
   UPDATING: 'USER_UPDATING',
   WARN    : 'USER_WARN',
 };
 
-export function errorUser(error)  {return {type: userActions.ERROR,  payload: error}}
-export function updateUser(user)  {return {type: userActions.UPDATE, payload: user}}
-export function updatingUser()    {return {type: userActions.UPDATING}}
-export function warnUser(warning) {return {type: userActions.WARN,   payload: warning}}
+export function errorUser(error)  {return {type: userAction.ERROR,  payload: error}}
+export function updateUser(user)  {return {type: userAction.UPDATE, payload: user}}
+export function updatingUser()    {return {type: userAction.UPDATING}}
+export function warnUser(warning) {return {type: userAction.WARN,   payload: warning}}
 
 export function userReducer(state = {updatingUser: false, user: {locale: 'en-US'}}, action) {
   let returnObject = null;
   switch (action.type) {
     // Error on Portfolio action.
-    case userActions.ERROR: {
+    case userAction.ERROR: {
       const {prefix, error} = action.payload;
       alert(Fmt.serverError(prefix, error));
       returnObject = Object.assign({}, state, {updatingUser: false});
       break;
     }
     // Update one User.
-    case userActions.UPDATE: {
+    case userAction.UPDATE: {
       const payloadUser = action.payload;
       returnObject = Object.assign({}, state, {updatingUser: false, user: payloadUser.data.attributes});
       break;
     }
     // Show that User is being modified.
-    case userActions.UPDATING:
+    case userAction.UPDATING:
       returnObject = Object.assign({}, state, {updatingUser: true});
       break;
     // Warning on User action.
-    case userActions.WARN: {
+    case userAction.WARN: {
       const {prefix, warning} = action.payload;
       alert(Fmt.serverError(prefix, warning));
       returnObject = state;
