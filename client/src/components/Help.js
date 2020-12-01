9fbb4a4b635dd16c80a90f6ae9720d1e8a17a3c1
jcpny1 <>
2020-11-30 23:13:03 -0500

    Update help page.

--- client/src/components/Help.js
+++ client/src/components/Help.js
@@ -5,7 +5,7 @@ export const Help = () => {
     <div>
       <h3>No Investment Advice or Recommendations</h3>
       <ul>
-        <li>The content of the Portfolio-Analyzer app is for education and entertainment purposes only.</li>
+        <li>The content of the Portfolio Analyzer app is for educational and entertainment purposes only.</li>
         <li>No decisions should be based on this app.</li>
         <li>Any information contained herein should be verfied through other sources.</li>
       </ul>
@@ -20,7 +20,7 @@ export const Help = () => {
         <li>Index data (specifically, the DJIA) is provided by <a href='https://www.alphavantage.co' target='_blank' rel='noopener noreferrer'>Alpha Vantage</a>.</li>
         <li>Note that the prices shown may not be the correct prices or the latest prices.</li>
         <li>Any prices displayed should be verfied through other sources.</li>
-        <li>When the application is first opened, the prices displayed are from the last time someone refreshed a portfolio containing those same symbols. Pressing refresh will update those prices from the latest available prices.</li>
+        <li>When the application is first opened, the prices displayed are from the last time someone refreshed a portfolio containing those same symbols. Pressing refresh will update those prices with the latest available prices.</li>
         <li>Day change values are calculated based on the previous close price, not the current open price.</li>
       </ul>
       <h3>Portfolio Charts</h3>
@@ -41,14 +41,14 @@ export const Help = () => {
       </ul>
       <h3>Known issues:</h3>
       <ul>
-        <li>Chart data is not provided by the feed vendor for every instrument.</li>
+        <li>The feed vendor does not provide chart data for all instruments.</li>
         <li>
-          Due to vendor call rate problems, series data will only call for new data when the latest series data point date or price does not match the corresponding latest trade data.
+          Due to vendor call rate limitations, series data will only call for new data when the latest saved series data point date or price does not match the corresponding latest saved trade data.
           To only fetch missing series values, rerun Refresh Series one or more times without running Refresh Prices.
           To update all series data, run Refresh Prices first.
         </li>
         <li>Chart data provided by the feed vendor may or may not include the latest trading day.</li>
-        <li>Due to license restrictions, Headline News may post an error popup if the request limit is reached.</li>
+        <li>Due to license restrictions, Headline News may post an error popup if the daily request limit is reached.</li>
         <li>Some server errors will display popups for debugging purposes. This is expected since this is a development environment.</li>
       </ul>
     </div>
