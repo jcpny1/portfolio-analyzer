b3b98ea10ed969ae60aab83adb496e1470e3f0cd
jcpny1 <jpfingst@outlook.com>
2020-12-12 20:57:50 -0500

    Remediate Rubocop warning.

--- spec/factories/trades.rb
+++ spec/factories/trades.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :trade do
-    instrument   {build(:instrument)}
+    instrument   { build(:instrument) }
     trade_date   { "2017-12-08" }
     trade_price  { 123.450 }
     price_change {   1.125 }
